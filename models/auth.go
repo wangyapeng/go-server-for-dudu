4c05eb55bc4248d4255a653c47ec5f84475f9230
wangyph@chanjet.com <wangyph@chanjet.com>
2023-09-11 11:22:40 +0800

    chore: 删除www部分，改为纯后端服务工程

--- models/auth.go
+++ models/auth.go
@@ -1,11 +1,5 @@
 package models
 
-import "fmt"
-
-func Test3() {
-	fmt.Println("test3 execute")
-}
-
 func init() {
 
 }
--- routers/router.go
+++ routers/router.go
@@ -6,30 +6,8 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 )
 
-func intercepter() {
-
-	// beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-	// 	AllowAllOrigins:  true,
-	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTION"},
-	// 	AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-	// 	ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-	// 	AllowCredentials: true,
-	// }))
-}
-
 // init
 func init() {
-
-	intercepter()
-
-	// 用户
-	// beego.CtrlGet("v1/user", (*controllers.UserController).Register)
-
-	beego.CtrlGet("/", (*controllers.HomeController).GetTemplate)
-
-	// 登录静态页面
-	beego.CtrlGet("/login", (*controllers.HomeController).GetTemplate)
-
 	// ---------------> rest posts -------------- //
 	beego.CtrlPost("v1/user/register", (*controllers.UserController).Register)
 	beego.CtrlGet("v1/user/login", (*controllers.UserController).Login)
